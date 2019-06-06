09349b8dc042ee33099e4b80aca3c4536eb2840d
snytav <snytav@gmail.com>
2019-06-06 13:17:11 +0700

    symbolic integral for A(i,j)

--- 1D/1_poisson/gauss/asym.py
+++ 1D/1_poisson/gauss/asym.py
@@ -1,4 +1,4 @@
-import sympy
+import sympy 
 import numpy as np
 
 def phi(i,n):
@@ -18,8 +18,46 @@ def phi(i,n):
         print(j)
         ph.append(0.0)
 
-    return ph 
+    return ph
+
+def get_integration_functions(i,j):
+    fi = phi(i,n)
+    fj = phi(j,n)
+    print(fi)
+    print(fj)
+
+    ic = i
+    f1 = fi[ic]
+    f2 = fj[ic]
+    x,hx = sympy.symbols('x hx')
+
+    df1 = f1.diff(x)
+    df2 = f2.diff(x)
+    print(df1)
+    print(df2)
+ 
+    return [df1,df2]
+
+def integrate_on_FE(f1,f2,dx):
+    x,hx = sympy.symbols('x hx')
+    s = sympy.integrate(df1*df2,(x,0,hx))
+    print("integral ",s)
+    num = s.subs(hx,dx)
+    return num
+
+
 n = 10
 
-f = phi(2,n)
-print(f)
+i = 2
+j = 3
+
+df1,df2 = get_integration_functions(i,j)
+
+print(df1)
+print(df2)
+
+s = sympy.integrate(df1*df2,(x,0,hx))
+print("integral ",s)
+
+num = integrate_on_FE(df1,df2,0.25)
+print(num)
