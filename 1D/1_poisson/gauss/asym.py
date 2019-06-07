d5ce807e14ded24006a3cb2fca3ffffc2d1c4dcc
snytav <snytav@gmail.com>
2019-06-07 19:59:59 +0700

    checked matrix: need fix korner values

--- 1D/1_poisson/gauss/asym.py
+++ 1D/1_poisson/gauss/asym.py
@@ -1,15 +1,16 @@
 import sympy 
 import numpy as np
+import sys
 
 def phi(i,n):
     #xx = np.linspace(0,1,n)
     x,hx = sympy.symbols('x hx')
 
     ph = []
-    #print('function num ',i)
+    ##print('function num ',i)
     for j in range(0,i):
         ph.append(0.0)
-    #print(ph)
+    ##print(ph)
      
     y =  x/hx
     ph.append(y)
@@ -17,9 +18,9 @@ def phi(i,n):
     ph.append(y)
 
     for j in range(i+2,n):
-#        #print(j)
+#        ##print(j)
         ph.append(0.0)
-    #print(len(ph))
+    ##print(len(ph))
     return ph
 
 # i,j - numbers of basis functions
@@ -27,8 +28,8 @@ def phi(i,n):
 def get_integration_functions(i,j,num_fe):
     fi = phi(i,n)
     fj = phi(j,n)
-    #print(fi)
-    #print(fj)
+    #print('fi ',i,fi)
+    #print('fj ',j,fj)
 
     ic = i
     f1 = fi[num_fe]
@@ -49,15 +50,15 @@ def get_integration_functions(i,j,num_fe):
     else:
        df2 = f2.diff(x)
 
-    #print(df1)
-    #print(df2)
+    #print('df1 ',i,df1)
+    #print('df2 ',j,df2)
  
     return [df1,df2]
 
 def integrate_on_FE(f1,f2,dx):
     x,hx = sympy.symbols('x hx')
     s = sympy.integrate(f1*f2,(x,0,hx))
-#    #print("integral ",s)
+    #print("integral ",s)
     num = s.subs(hx,dx)
     return num
 
@@ -70,6 +71,9 @@ def integrate_two_functions(i,j,x,ic,prep):
 
 
 def get_stiffness_element(i,j,x,prep):
+    if abs(i - j) > 1:
+       return 0.0
+
     n1  = integrate_two_functions(i,j,x,i,prep)
     n2  = integrate_two_functions(i,j,x,j,prep)
 
@@ -87,19 +91,40 @@ def get_stiffness_element(i,j,x,prep):
 
 n = 23
 x = np.linspace(0,1,n)
-##print(x)
+###print(x)
 i = 1
 j = 2
 
 num = get_stiffness_element(i,i,x,get_integration_functions)
-print('result ii ',num)
-#print('##################################################')
+#print('result ii ',num)
+##print('##################################################')
 num = get_stiffness_element(j,j,x,get_integration_functions)
-print('result jj ',num)
+#print('result jj ',num)
 
 num = get_stiffness_element(i,j,x,get_integration_functions)
-print('result ij ',num)
-#print('##################################################')
+#print('result ij ',num)
+##print('##################################################')
 num = get_stiffness_element(j,i,x,get_integration_functions)
-print('result ji ',num)
+#print('result ji ',num)
 
+A = np.zeros((n,n))
+
+for i in range(0,n-1):
+    for j in range(0,n-1):
+        #print(i,j,'#############################################')
+        a = get_stiffness_element(i,j,x,get_integration_functions)
+        ##print(a)
+        ##print(type(a))
+        #sys.exit()
+        if i == j:
+           A[i][i]     += a[0]
+           if i < n-2:
+              A[i+1][i+1] += a[1] 
+        else:   
+           #print(i,j,a)  
+           A[i][j] += a
+        #print(i,j,A[i][j])
+       
+
+np.savetxt('Asym.txt',A,fmt='%.2f')
+ 
